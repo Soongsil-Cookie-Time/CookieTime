a32b7e5748acb7989e9a1a8b5b909474bc602134
Jiwoong CHOI <jwchoi179@soongsil.ac.kr>
2024-11-19 20:23:48 +0900

    chore: 사용자별 게시글을 올리기 위한 버튼 리스너 연결

--- app/src/main/java/com/ssuclass/cookietime/ui/community/CommunityWriteActivity.java
+++ app/src/main/java/com/ssuclass/cookietime/ui/community/CommunityWriteActivity.java
@@ -1,21 +1,12 @@
 package com.ssuclass.cookietime.ui.community;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.ssuclass.cookietime.databinding.ActivityCommunityWriteBinding;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
 
 public class CommunityWriteActivity extends AppCompatActivity {
 
@@ -40,28 +31,7 @@ public class CommunityWriteActivity extends AppCompatActivity {
         binding.toolbarDoneButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Map<String, Object> communities = new HashMap<>();
-
-                String uuid = UUID.randomUUID().toString();
-                String title = binding.titleEditext.getText().toString();
-
-                communities.put(uuid, title);
-
-                db.collection("Communities")
-                        .add(communities)
-                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                            @Override
-                            public void onSuccess(DocumentReference documentReference) {
-                                Log.d("FirebasePushDebug", documentReference.getId());
-                            }
-                        })
-                        .addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                Log.e("FirebasePushDebug", e.toString());
-                            }
-                        });
-
+                // TODO: 사용자가 별 작성한 글을 어떻게 분간할 것인지 확인, DB 구성을 어떻게 가져가야 하는지 연구
             }
         });
     }
