9537412f7b7fa9b508764ff7a14214faff50ed92
Jiwoong CHOI <jwchoi179@soongsil.ac.kr>
2024-11-20 18:34:50 +0900

    feat: InstagramSharingActivity 연결

--- app/src/main/java/com/ssuclass/cookietime/presentation/badgemanager/ItemMonthlyBadgesAdapter.java
+++ app/src/main/java/com/ssuclass/cookietime/presentation/badgemanager/ItemMonthlyBadgesAdapter.java
@@ -1,6 +1,9 @@
 package com.ssuclass.cookietime.presentation.badgemanager;
 
+import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
@@ -9,6 +12,13 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.ssuclass.cookietime.databinding.ItemBadgeBinding;
 
 public class ItemMonthlyBadgesAdapter extends RecyclerView.Adapter<ItemMonthlyBadgesAdapter.ItemMonthlyBadgesViewHolder> {
+
+    private final Context context;
+
+    public ItemMonthlyBadgesAdapter(Context context) {
+        this.context = context;
+    }
+
     @NonNull
     @Override
     public ItemMonthlyBadgesViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -18,7 +28,13 @@ public class ItemMonthlyBadgesAdapter extends RecyclerView.Adapter<ItemMonthlyBa
 
     @Override
     public void onBindViewHolder(@NonNull ItemMonthlyBadgesViewHolder holder, int position) {
-        // TODO: 실질적으로 배지 처리되는 코드
+        holder.binding.getRoot().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, InstagramSharingActivity.class);
+                context.startActivity(intent);
+            }
+        });
     }
 
     @Override
