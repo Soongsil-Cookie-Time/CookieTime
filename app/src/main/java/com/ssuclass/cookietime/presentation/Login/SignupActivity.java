c766ff298085e90d9c7f1d4a195ce5432a089de4
Jiwoong CHOI <jwchoi179@soongsil.ac.kr>
2024-11-25 21:09:59 +0900

    refactor: call-back 코드 함수로 분리화

--- app/src/main/java/com/ssuclass/cookietime/presentation/Login/SignupActivity.java
+++ app/src/main/java/com/ssuclass/cookietime/presentation/Login/SignupActivity.java
@@ -16,12 +16,14 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.ssuclass.cookietime.databinding.ActivitySignupBinding;
 
 public class SignupActivity extends AppCompatActivity {
 
     ActivitySignupBinding binding;
     FirebaseAuth mAuth;
+    FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,12 +37,13 @@ public class SignupActivity extends AppCompatActivity {
             return insets;
         });
 
-        addFirebaseAuthInstance();
+        addFirebaseInstance();
         addButtonListener();
     }
 
-    private void addFirebaseAuthInstance() {
+    private void addFirebaseInstance() {
         mAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
     }
 
     private void addButtonListener() {
@@ -53,55 +56,66 @@ public class SignupActivity extends AppCompatActivity {
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
                             if (task.isSuccessful()) {
-                                AlertDialog.Builder builder = new AlertDialog.Builder(SignupActivity.this);
-                                builder.setTitle("알림")
-                                        .setMessage("회원가입에 성공했습니다!")
-                                        .setPositiveButton("확인", new DialogInterface.OnClickListener() {
-                                            @Override
-                                            public void onClick(DialogInterface dialogInterface, int i) {
-                                                AlertDialog.Builder signinDialogBuilder = new AlertDialog.Builder(SignupActivity.this);
-                                                signinDialogBuilder
-                                                        .setTitle("알림")
-                                                        .setMessage("로그인 하시겠습니까?")
-                                                        .setPositiveButton("확인", new DialogInterface.OnClickListener() {
-                                                            @Override
-                                                            public void onClick(DialogInterface dialogInterface, int i) {
-                                                                // TODO: Firestore에 개별 사용자 정보 보관 로직 구현
-                                                                Intent intent = new Intent(SignupActivity.this, SigninActivity.class);
-                                                                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                                                                startActivity(intent);
-                                                                finish();
-                                                            }
-                                                        })
-                                                        .setNegativeButton("취소", new DialogInterface.OnClickListener() {
-                                                            @Override
-                                                            public void onClick(DialogInterface dialogInterface, int i) {
-                                                                Intent intent = new Intent(SignupActivity.this, AuthentiacationActivity.class);
-                                                                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                                                                startActivity(intent);
-                                                                finish();
-                                                            }
-                                                        })
-                                                        .show();
-                                            }
-                                        })
-                                        .show();
+                                makeSignupSuccessDialog();
                             } else {
-                                AlertDialog.Builder builder = new AlertDialog.Builder(SignupActivity.this);
-                                builder
-                                        .setTitle("알림")
-                                        .setMessage("회원가입에 실패했습니다.\n다시 시도하세요.")
-                                        .setPositiveButton("확인", new DialogInterface.OnClickListener() {
-                                            @Override
-                                            public void onClick(DialogInterface dialogInterface, int i) {
-                                                // TODO: 확인버튼 이후 액션이 필요없는데도, 꼭 작성해야 하는 코드인지?
-                                            }
-                                        })
-                                        .show();
+                                makeSignupFailureDialog();
                             }
                         }
                     });
         });
     }
 
+    private void makeSignupSuccessDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(SignupActivity.this);
+        builder.setTitle("알림")
+                .setMessage("회원가입에 성공했습니다!")
+                .setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        // TODO: Firestore에 개별 사용자 정보 보관 로직 구현
+                        makeSigninRequestDialog();
+                    }
+                })
+                .show();
+    }
+
+    private void makeSignupFailureDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(SignupActivity.this);
+        builder
+                .setTitle("알림")
+                .setMessage("회원가입에 실패했습니다.\n다시 시도하세요.")
+                .setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        // TODO: 확인버튼 이후 액션이 필요없는데도, 꼭 작성해야 하는 코드인지?
+                    }
+                })
+                .show();
+    }
+
+    private void makeSigninRequestDialog() {
+        AlertDialog.Builder signinDialogBuilder = new AlertDialog.Builder(SignupActivity.this);
+        signinDialogBuilder
+                .setTitle("알림")
+                .setMessage("로그인 하시겠습니까?")
+                .setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Intent intent = new Intent(SignupActivity.this, SigninActivity.class);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(intent);
+                        finish();
+                    }
+                })
+                .setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Intent intent = new Intent(SignupActivity.this, AuthentiacationActivity.class);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(intent);
+                        finish();
+                    }
+                })
+                .show();
+    }
 }
