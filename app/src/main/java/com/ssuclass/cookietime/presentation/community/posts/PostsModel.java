7835cbf3085cbaccc5a48e3725ac4181a53b9e68
Jiwoong CHOI <jwchoi179@soongsil.ac.kr>
2024-11-29 18:19:26 +0900

    feat: 게시글 작성 비즈니스 로직 설계 완료

--- app/src/main/java/com/ssuclass/cookietime/presentation/community/posts/PostsModel.java
+++ app/src/main/java/com/ssuclass/cookietime/presentation/community/posts/PostsModel.java
@@ -3,8 +3,32 @@ package com.ssuclass.cookietime.presentation.community.posts;
 public class PostsModel {
     private String title;
     private String postId;
+    private String content;
+    private String nickname;
 
-    // Getter와 Setter
+    public PostsModel(String title, String postId, String content, String nickname) {
+        this.nickname = nickname;
+        this.content = content;
+        this.title = title;
+        this.postId = postId;
+    }
+
+    public String getNickname() {
+        return this.nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getContent() {
+        return this.content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
     public String getTitle() {
         return title;
     }
--- app/src/main/java/com/ssuclass/cookietime/util/FirebaseConstants.java
+++ app/src/main/java/com/ssuclass/cookietime/util/FirebaseConstants.java
@@ -2,7 +2,10 @@ package com.ssuclass.cookietime.util;
 
 public class FirebaseConstants {
     public static final String USERS_COLLECTION = "User";
+    public static final String COMMUNITIES_COLLECTION = "Communities";
     public static final String COOKIE_COLLECTION = "Cookie";
     public static final String POSTS_COLLECTION = "Posts";
     public static final String COMMENTS_COLLECTION = "Comments";
+
+    public static final String MOVIEID_DOCUMENT = "movieId";
 }
