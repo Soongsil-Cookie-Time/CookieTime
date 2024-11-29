48fd93bf7a8c687c47f7285dcf8ade6c73e5b8fc
Jiwoong CHOI <jwchoi179@soongsil.ac.kr>
2024-11-29 16:02:33 +0900

    feat: 닉네임 변경 & 비밀번호 변경 비즈니스 로직 연결

--- app/src/main/java/com/ssuclass/cookietime/presentation/mypage/change/nickname/ChangeNicknameActivity.java
+++ app/src/main/java/com/ssuclass/cookietime/presentation/mypage/change/nickname/ChangeNicknameActivity.java
@@ -3,24 +3,70 @@ package com.ssuclass.cookietime.presentation.mypage.change.nickname;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.ssuclass.cookietime.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.ssuclass.cookietime.databinding.ActivityChangeNicknameBinding;
+import com.ssuclass.cookietime.util.FirebaseConstants;
+import com.ssuclass.cookietime.util.ToastHelper;
 
 public class ChangeNicknameActivity extends AppCompatActivity {
+
+    ActivityChangeNicknameBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_change_nickname);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        binding = ActivityChangeNicknameBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        ViewCompat.setOnApplyWindowInsetsListener(binding.getRoot(), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        addButtonListener();
+    }
+
+    private void addButtonListener() {
+        binding.changeButton.setOnClickListener(view -> {
+            FirebaseUser user;
+            String uid = null;
+
+            try {
+                user = FirebaseAuth.getInstance().getCurrentUser();
+                uid = user.getUid();
+            } catch (RuntimeException e) {
+                ToastHelper.showToast(ChangeNicknameActivity.this, "사용자 정보를 찾지 못했습니다.");
+            }
+
+            FirebaseFirestore db = FirebaseFirestore.getInstance();
+            db.collection(FirebaseConstants.USERS_COLLECTION)
+                    .document(uid)
+                    .update("nickname", binding.inputChangeNicknameEdittext.getText().toString())
+                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void unused) {
+                            ToastHelper.showToast(ChangeNicknameActivity.this, "닉네임 변경에 성공했습니다.");
+                            finish();
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            ToastHelper.showToast(ChangeNicknameActivity.this, "닉네임 변경에 실패했습니다.");
+                        }
+                    });
+
         });
     }
 }
--- app/src/main/java/com/ssuclass/cookietime/presentation/mypage/change/password/ChangePasswordActivity.java
+++ app/src/main/java/com/ssuclass/cookietime/presentation/mypage/change/password/ChangePasswordActivity.java
@@ -8,19 +8,62 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.ssuclass.cookietime.R;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.ssuclass.cookietime.databinding.ActivityChangePasswordBinding;
+import com.ssuclass.cookietime.util.ToastHelper;
 
 public class ChangePasswordActivity extends AppCompatActivity {
+
+    ActivityChangePasswordBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_change_password);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        binding = ActivityChangePasswordBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        ViewCompat.setOnApplyWindowInsetsListener(binding.getRoot(), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        addButtonListener();
+    }
+
+    private void addButtonListener() {
+        binding.changeButton.setOnClickListener(view -> {
+            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+            if (user == null) {
+                ToastHelper.showToast(this, "사용자 정보를 찾지 못했습니다.");
+                return;
+            }
+
+            // 현재 비밀번호로 재인증
+            String email = user.getEmail();
+            String currentPassword = binding.inputCurrentPasswordEditext.getText().toString();
+            AuthCredential credential = EmailAuthProvider.getCredential(email, currentPassword);
+
+            user.reauthenticate(credential)
+                    .addOnSuccessListener(unused -> {
+                        // 재인증 성공 후 새 비밀번호로 업데이트
+                        String newPassword = binding.inputChangePasswordEditext.getText().toString();
+                        user.updatePassword(newPassword)
+                                .addOnSuccessListener(unused2 -> {
+                                    ToastHelper.showToast(ChangePasswordActivity.this,
+                                            "비밀번호 변경에 성공했습니다.");
+                                    finish();
+                                })
+                                .addOnFailureListener(e ->
+                                        ToastHelper.showToast(ChangePasswordActivity.this,
+                                                "비밀번호 변경에 실패했습니다."));
+                    })
+                    .addOnFailureListener(e ->
+                            ToastHelper.showToast(ChangePasswordActivity.this,
+                                    "현재 비밀번호가 일치하지 않습니다."));
         });
     }
 }
