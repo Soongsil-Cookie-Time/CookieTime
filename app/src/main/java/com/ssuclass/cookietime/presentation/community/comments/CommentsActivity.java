89ab417d74e6720d9ce586c1f41d8324b545c59d
Jiwoong CHOI <jwchoi179@soongsil.ac.kr>
2024-12-06 10:47:56 +0900

    feat: 키보드 레이아웃 추가

--- app/src/main/java/com/ssuclass/cookietime/presentation/community/comments/CommentsActivity.java
+++ app/src/main/java/com/ssuclass/cookietime/presentation/community/comments/CommentsActivity.java
@@ -4,11 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -37,14 +33,8 @@ public class CommentsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         binding = ActivityCommentsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        ViewCompat.setOnApplyWindowInsetsListener(binding.getRoot(), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
         getDatabaseData();
         setFirebaseInstance();
